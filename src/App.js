2c8ebc179914a9335e595dd1cbefcc6195eae8b2
Hugo Juca <hugojucah@gmail.com>
2022-10-13 23:37:11 +0100

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>hello zx</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
